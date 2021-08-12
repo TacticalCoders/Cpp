c536cc70f0ac2aecef1f50dc1131871edd1b10a1
littlebuffett <76998855+littlebuffett@users.noreply.github.com>
2021-08-13 07:30:52 +0900

    Create a)18 논리 표현식.cpp

---
+++ Study/a)18 논리 표현식.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	//논리 표현식
+	//논리합, 논리곱, 논리부정 연산자
+
+	// 논리합 ||
+	//좌항 || 우항 -> 하나라도 true이면 true반환, 모두 거짓이면 false.
+	
+	// 논리곱 && 
+	// 좌항 && 우항 -> 모두 true이면 true, 나머지 경우는 모두 false.
+
+	// 논리부정 !
+	// !(표현식) -> 표현식이 true이면 false, false이면 true 반환!
+
+	return 0;
+}
