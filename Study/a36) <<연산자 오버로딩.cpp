a6f24d47fba0711ee852111de77a53d4c149da68
littlebuffett <76998855+littlebuffett@users.noreply.github.com>
2021-08-25 07:24:46 +0900

    Create a36) <<연산자 오버로딩.cpp

---
+++ Study/a36) <<연산자 오버로딩.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+class Time
+{
+private:
+	int hours;
+	int mins;
+
+public:
+	Time();
+	Time(int, int);
+	void addHours(int);
+	void addMins(int);
+	Time operator+(Time&);
+	Time operator*(int);
+	void show();
+	~Time();
+	friend Time operator*(int n, Time& t) {
+		return t * n;
+	}
+	friend std::ostream& operator<<(std::ostream&, Time&);
+};
+
+int main() {
+	// <<연산자 오버로딩 (스트링 추출 연산자)
+
+	Time t1(3, 45);
+	t1.show();
+	cout << t1;
+
+	return 0;
+}
+
+Time::Time()
+{
+	hours = mins = 0;
+}
+
+Time::Time(int h, int m) {
+	hours = h;
+	mins = m;
+}
+
+void Time::addHours(int h) {
+	hours += h;
+};
+
+void Time::addMins(int m) {
+	mins += m;
+	hours += mins / 60;
+	mins %= 60;
+};
+
+Time Time::operator+(Time& t) {
+	Time sum;
+	sum.mins = mins + t.mins;
+	sum.hours = hours + t.hours;
+	sum.hours += sum.mins / 60;
+	sum.mins %= 60;
+	return sum;
+};
+
+void Time::show() {
+	std::cout << "시간 : " << hours << std::endl;
+	std::cout << "분 : " << mins << std::endl;
+}
+
+Time::~Time()
+{
+}
+
+Time Time::operator*(int n) {
+	Time result;
+	long resultMin = hours * n * 60 + mins * n;
+	result.hours = resultMin / 60;
+	result.mins = resultMin % 60;
+	return result;
+}
+
+std::ostream& operator<<(std::ostream& os, Time& t) {
+	os << t.hours << "시간 " << t.mins << "분";
+	return os;
+}
